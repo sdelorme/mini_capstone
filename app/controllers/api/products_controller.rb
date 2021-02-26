a9cbe1a12724c06bdf98cb1f07dc3fd095b89f88
Seth Delorme <delorme.s.m@gmail.com>
2021-02-25 21:46:29 -0600

    creates update and destroy actions in controller and patch, destroy routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -18,9 +18,24 @@ class Api::ProductsController < ApplicationController
       name: params[:name], 
       price: params[:price], 
       image_url: params[:image_url], 
-      description: params[:description]
-    )
+      description: params[:description])
     @product.save
     render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url  
+    @product.description = params[:description] || @product.description 
+    @product.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: {message: "this product no longer exists"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
     post "products" => "products#create"
+    patch "products/:id" => "products#update"
+    delete "products/:id" => "products#destroy"
   end
   
 end
