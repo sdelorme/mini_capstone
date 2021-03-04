ca680aee47f587ff09c0aade16cff52910e69919
Seth Delorme <delorme.s.m@gmail.com>
2021-03-03 20:00:46 -0600

    adds three new validations to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
-  def is_discounted?
+  validates :name, presence: true, uniqueness: true
+  validates :price, numericality: true, numericality: {greater_than: 0}
+  validates :description, length: {in: 10..500}
+
+  def is_discounted?    
     if price < 10
       return true
     else
