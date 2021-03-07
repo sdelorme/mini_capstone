c6ef15f2a9940f26cfad00243af15cd913381348
Seth Delorme <delorme.s.m@gmail.com>
2021-03-07 12:14:51 -0600

    adds index action to suppliers controller and views for that page

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -1,2 +1,18 @@
 class Api::SuppliersController < ApplicationController
+  def index
+    @suppliers = Supplier.all
+    render 'index.json.jb'
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,8 @@ class Product < ApplicationRecord
   validates :price, numericality: {greater_than: 0}
   validates :description, length: {in: 10..500}
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
+
   def is_discounted?    
     if price < 10
       return true
