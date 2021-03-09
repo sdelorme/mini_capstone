5f78346b64685373b2c043f695a7e3f6de29a2f4
Seth Delorme <delorme.s.m@gmail.com>
2021-03-08 21:04:24 -0600

    creates users table in db, creates sessions table in db, adds create action in users controller, adds JWT action in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,16 @@ Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
-    post "products" => "products#create"
-    patch "products/:id" => "products#update"
-    delete "products/:id" => "products#destroy"
+    post "/products" => "products#create"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
     get "/suppliers" => "suppliers#index"
     get "/suppliers/:id" => "suppliers#show"
-    post "suppliers" => "suppliers#create"
-    patch "suppliers/:id" => "suppliers#update"
-    delete "suppliers/:id" => "suppliers#destroy"
+    post "/suppliers" => "suppliers#create"
+    patch "/suppliers/:id" => "suppliers#update"
+    delete "/suppliers/:id" => "suppliers#destroy"
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
   end
   
 end
