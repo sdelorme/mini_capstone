628f0c00e7b0ceedd72f4186f480d60255516ca5
Seth Delorme <delorme.s.m@gmail.com>
2021-03-08 19:26:55 -0600

    adds images model and foreign prodcut_id key to images table

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,8 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, numericality: {greater_than: 0}
   validates :description, length: {in: 10..500}
-
   belongs_to :supplier
-
+  has_many :images
   def is_discounted?    
     if price < 10
       return true
