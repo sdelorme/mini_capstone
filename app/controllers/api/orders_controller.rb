a1e68ed6e3f11cd43ffe0150a551e779dc73ea35
Seth Delorme <delorme.s.m@gmail.com>
2021-03-09 21:28:06 -0600

    refactor index and show views for orders

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,15 +1,20 @@
 class Api::OrdersController < ApplicationController
   def create
-    @order = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: Product.find_by(id: params[:product_id]).price,
-      tax: Product.find_by(id: params[:product_id]).tax,
-      total: Product.find_by(id: params[:product_id]).total
-    )
-    @order.save
-    render 'show.json.jb'
+    if current_user
+      @product = Product.find_by(id: params[:product_id])
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: Product.find_by(id: params[:product_id]).price,
+        tax: @product.tax,
+        total: @product.total
+      )
+      @order.save
+      render 'show.json.jb'
+    else
+      render json: {message: "Sign in or signup to create an order!"}
+    end
   end
   def index
     if current_user
