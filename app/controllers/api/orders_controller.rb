01c1061b6bc8be3276935fbdde4b66e4220b1261
Seth Delorme <delorme.s.m@gmail.com>
2021-03-09 21:09:38 -0600

    creates index action in orders controller, creates index view for orders, creates index route for orders

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -11,4 +11,12 @@ class Api::OrdersController < ApplicationController
     @order.save
     render 'show.json.jb'
   end
+  def index
+    if current_user
+      @orders = Order.where(user_id: current_user.id)
+      render 'index.json.jb'
+    else
+      render json: {message: "Nice try, you have no orders"}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
     post "/orders" => "orders#create"
+    get "/orders" => "orders#index"
   end
   
 end
