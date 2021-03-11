8e67a0d8009bf9a4c01650dfa8a8867ef39c689c
Seth Delorme <delorme.s.m@gmail.com>
2021-03-10 18:51:42 -0600

    refactors create action in orders controller to calculate correct subtotal for quantities higher than 1 of a product

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -2,13 +2,17 @@ class Api::OrdersController < ApplicationController
   def create
     if current_user
       @product = Product.find_by(id: params[:product_id])
+      calculated_subtotal = params[:quantity].to_i * @product.price
+      # below is however many user puts in for quant and taking the tax of one of those products from product tax method
+      calculated_tax = params[:quantity].to_i * @product.tax
+      calculated_total = calculated_subtotal + calculated_tax
       @order = Order.new(
         user_id: current_user.id,
         product_id: params[:product_id],
         quantity: params[:quantity],
-        subtotal: Product.find_by(id: params[:product_id]).price,
-        tax: @product.tax,
-        total: @product.total
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total
       )
       @order.save
       render 'show.json.jb'
