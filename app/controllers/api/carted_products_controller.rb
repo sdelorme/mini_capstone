dd47be2c96719899cea9eca9a825a7343aaca626
Seth Delorme <delorme.s.m@gmail.com>
2021-03-14 11:54:31 -0500

    create index and create action for CartedProducts

---
+++ app/controllers/api/carted_products_controller.rb
@@ -0,0 +1,15 @@
+class Api::CartedProductsController < ApplicationController
+  def create
+    @carted_product = CartedProduct.new(
+    user_id: current_user.id,
+    product_id: params[:product_id],
+    quantity: params[:quantity],
+    )
+    @carted_product.save
+    render 'show.json.jb'
+  end
+  def index
+    @carted_products = current_user.carted_products.where(status: "carted")
+    render 'index.json.jb'
+  end
+end
