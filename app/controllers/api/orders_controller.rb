62b8e408560fecb3c4ad37080709276426b29f20
Seth Delorme <delorme.s.m@gmail.com>
2021-03-15 18:16:03 -0500

    refactor

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -2,18 +2,18 @@ class Api::OrdersController < ApplicationController
   before_action :authenticate_user 
 
   def create
-    @product = Product.find_by(id: params[:product_id])
-    calculated_subtotal = params[:quantity].to_i * @product.price
-    # below is however many user puts in for quant and taking the tax of one of those products from product tax method
-    calculated_tax = params[:quantity].to_i * @product.tax
-    calculated_total = calculated_subtotal + calculated_tax
+    # @product = Product.find_by(id: params[:product_id])
+    # calculated_subtotal = params[:quantity].to_i * @product.price
+    # # below is however many user puts in for quant and taking the tax of one of those products from product tax method
+    # calculated_tax = params[:quantity].to_i * @product.tax
+    # calculated_total = calculated_subtotal + calculated_tax
+    @carted_products = CartedProduct.where(status: "carted", user_id: current_user.id)
+  
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total
+      subtotal: @order.subtotal,
+      tax: 1000,
+      total: 1000
     )
     @order.save
     render 'show.json.jb'
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :carted_product
+  has_many :products, through: :carted_products
+
+  def subtotal
+    carted_products.each do |carted_product|
+      price = carted_product.product.price
+      quantity = carted_product.quantity
+      return price * quantity
+    end
+  end
+  
+  
 end
